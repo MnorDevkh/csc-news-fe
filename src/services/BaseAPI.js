e0b9e38de22a801f4a005dd8d6df21d4a6085d6c
2005 <96278125+MnorDevkh@users.noreply.github.com>
2026-02-24 14:52:06 +0700

    Update BaseAPI.js

--- src/services/BaseAPI.js
+++ src/services/BaseAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class BaseAPI {
   constructor() {
     // Define the base URL for all services
-    this.baseURL = import.meta.env.VITE_API_URL || 'https://csc-news-api.boeurnmnor.site/api/v1';
+    this.baseURL = import.meta.env.VITE_API_URL;
 
     // 1. Public Client: For requests that do not require authentication headers
     this.publicClient = axios.create({
